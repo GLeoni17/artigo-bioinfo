3e0e245b795e74a97005dbe270e9dc8a2da0298d
Azure <ppcardana@hotmail.com>
2025-05-10 13:54:49 -0300

    funcionalidade movida para get_dataset.py

--- base_sequence.py
+++
@@ -1,7 +0,0 @@
-from Bio import Entrez, SeqIO
-
-Entrez.email = "lahed97318@exemplo.com"
-handle = Entrez.efetch(db="nucleotide", id="NC_045512.2", rettype="fasta", retmode="text")
-record = SeqIO.read(handle, "fasta")
-SeqIO.write(record, "wuhan_reference.fasta", "fasta")
-handle.close()
